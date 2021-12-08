e9fafd74d5f21c7d1d4fe260bc333f176d6f8ecb
Andrew Lee <andrew.lee@hardcoretech.co>
2021-12-09 03:51:39 +0800

    config(settings):  setting admin email

--- fbdjexam/settings.py
+++ fbdjexam/settings.py
@@ -14,6 +14,10 @@ import os
 from pathlib import Path
 import django_heroku
 
+ADMINS = [
+    ('icycandle', 'icycandle@gmail.com'),
+]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
